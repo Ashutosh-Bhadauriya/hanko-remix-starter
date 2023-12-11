2e06894c1ae9cc6419f9bf462c74ada8a1c89071
“Ashutosh-Bhadauriya” <ashutoshb455@gmail.com>
2023-12-11 22:03:37 +0530

    minor fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useNavigate } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,9 +9,11 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Hello</h1>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8", display: "flex", justifyContent: "center", alignItems:"center", marginTop: "100px"}}>
+      <button onClick={() => navigate("/login")}>Login</button>
     </div>
   );
 }
